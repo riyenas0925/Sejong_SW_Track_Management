f6c5add7ca55a12d073d3628444b74f799cca00f
2kyung19 <2kyung19@gmail.com>
2018-12-09 02:14:09 +0900

    전체 트랙 데이터 정렬

--- project/Server/home/views.py
+++ project/Server/home/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from .models import TrackList
+from .models import BsubList
+from .models import AsubList
 
+import datetime
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'home/home.html', {'posts': posts})
+	tracklists = TrackList.objects.all()
+	bsublists=BsubList.objects.all()
+	asublists=AsubList.objects.all()
+
+	posts = Post.objects.filter(published_date__lte=timezone.now())
+
+	context= { 'posts' : posts , 'tracklists':tracklists, 'bsublists':bsublists,'asublists':asublists}
+
+	return render(request, 'home/home.html',context)
