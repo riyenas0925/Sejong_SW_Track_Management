10331a5396975e1624b7720fbca0ee94cf501847
riyenas0925 <riyenas0925@gmail.com>
2018-12-09 00:21:08 +0900

    adminlte v2 적용 및 기본 템플릿(base.html), 확장 템플릿(home.html)

--- project/Server/home/views.py
+++ project/Server/home/views.py
@@ -6,4 +6,4 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'home/post_list.html', {'posts': posts})
+    return render(request, 'home/home.html', {'posts': posts})
