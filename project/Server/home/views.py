b1485f8a12bdec4c091fad348f40939b4e4bcde5
2kyung19 <2kyung19@gmail.com>
2018-12-09 14:40:49 +0900

    필터 오류

--- project/Server/home/views.py
+++ project/Server/home/views.py
@@ -8,10 +8,24 @@ import datetime
 # Create your views here.
 
 def post_list(request):
+	tag_filterA=[]
+	tag_filterB=[]
+
 	tracks = Track.objects.all()
 	sublists = SubList.objects.all()
-
 	posts = Post.objects.filter(published_date__lte=timezone.now())
+	
+	for i in range(0,10):
+		tag_filterA[i]=sublists[i].tagA.filter(title__icontains=tracks[i].tag)
+		tag_filterB[i]=sublists[i].tagB.filter(title__icontains=tracks[i].tag)
 
-	context= { 'posts' : posts , 'tracks':tracks, 'sublists':sublists }
+	context= { 'posts' : posts , 
+			   'tracks':tracks, 
+			   'sublists':sublists,
+			   'tag_filterA':tag_filterA,
+			   'tag_filterB':tag_filterB,
+	}
 	return render(request, 'home/home.html',context)
+
+def tag_filter():
+	return 0
