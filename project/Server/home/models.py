a80dc69bbcf7e93b9176e9d24c9b2b653d18d049
riyenas0925 <riyenas0925@gmail.com>
2018-12-11 14:41:57 +0900

    메인페이지 내용 변경

--- project/Server/home/models.py
+++ project/Server/home/models.py
@@ -3,33 +3,32 @@ from django.utils import timezone
 
 # Create your models here.
 
-##########전체 트랙 정보###########
 class TrackList(models.Model):
-    tname=models.CharField(max_length=15)
-    tnum=models.IntegerField(default=0)
+    tname = models.CharField(max_length=15)
+    tnum = models.IntegerField(default=0)
 
     def __str__(self):
         return self.tname
 
 class BsubList(models.Model):
-    tnum=models.IntegerField(default=0)
-    bname=models.CharField(max_length=15)
+    tnum = models.IntegerField(default=0)
+    bname = models.CharField(max_length=15)
 
     def __str__(self):
         return self.bname
 
 
 class AsubList(models.Model):
-    tnum=models.IntegerField(default=0)
-    aname=models.CharField(max_length=15)
+    tnum = models.IntegerField(default=0)
+    aname = models.CharField(max_length=15)
 
     def __str__(self):
         return self.aname
 
 class User(models.Model):
-    name=models.CharField(max_length=15)
-    number=models.CharField(max_length=10)
-    selectTrack=models.CharField(max_length=15)
+    name = models.CharField(max_length=15)
+    number = models.CharField(max_length=10)
+    selectTrack = models.CharField(max_length=15)
     
     def __str__(self):
         return self.name
