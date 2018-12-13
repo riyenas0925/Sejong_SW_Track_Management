542d5a21ff9f7d174b236fb3bebb4a0554636a72
riyenas0925 <riyenas0925@gmail.com>
2018-12-13 18:55:14 +0900

    챗봇-공지사항 기능 추가

--- project/Server/home/views.py
+++ project/Server/home/views.py
@@ -5,7 +5,8 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django import forms
 import django_excel as excel
-
+from bs4 import BeautifulSoup
+import urllib.request
 import json
 import requests
 
@@ -23,6 +24,96 @@ import datetime
 
 class UploadFileForm(forms.Form):
     file = forms.FileField()
+
+
+def import_data(request):
+    Sheet1.objects.all().delete()
+
+    if request.method == "POST":
+        form = UploadFileForm(request.POST,
+                              request.FILES)
+
+        if form.is_valid():
+
+
+            request.FILES['file'].save_book_to_database(
+                models=[Sheet1],
+                initializers=[None],
+                mapdicts=[{'년도': 'number',
+                           '학기': 'dummy',
+                           '학수번호':'dummy',
+                           '교과목명': 'subject',
+                           '이수구분': 'dummy',
+                           '교직영역': 'dummy',
+                           '선택영역': 'dummy',
+                           '학점': 'dummy',
+                           '평가방식': 'dummy',
+                           '등급': 'dummy',
+                           '평점': 'dummy',
+                           '개설학과코드': 'dummy'}]
+            )
+            return redirect('resultTrack')
+        else:
+            return HttpResponseBadRequest()
+    else:
+        form = UploadFileForm()
+    return render(
+        request,
+        'home/upload_form.html',
+        {
+            'form': form,
+            'title': 'Import excel data into database example',
+            'header': 'Please upload sample-data.xls:'
+        })
+
+
+def handson_table(request):
+    return excel.make_response_from_tables(
+        [Sheet1], 'handsontable.html')
+
+def post_list(request):
+    return render(request, 'home/main.html')
+
+
+def allTrack(request):
+    tracklists = TrackList.objects.all()
+    bsublists=BsubList.objects.all()
+    asublists=AsubList.objects.all()
+
+    posts = Post.objects.filter(published_date__lte=timezone.now())
+
+    context= { 'posts' : posts , 'tracklists':tracklists, 'bsublists':bsublists,'asublists':asublists}
+    return render(request,'home/allTrack.html',context)
+
+def resultTrack(request):
+
+    users=User.objects.all()
+    usersubs=Sheet1.objects.all()
+    tracklists = TrackList.objects.all()
+    bsublists=BsubList.objects.all()
+    asublists=AsubList.objects.all()
+
+    usersubs.update(number = request.user.get_username())  # 일괄 update 요청
+
+    context={'users':users,
+             'usersubs':usersubs,
+             'tracklists':tracklists,
+             'bsublists':bsublists,
+             'asublists':asublists,
+             }
+
+    return render(request, 'home/resultTrack.html',context)
+
+
+
+def test(request):
+    return render(request,'home/test.html')
+
+def notice(request):
+    posts = Post.objects.all().order_by('-created_date') #날짜 역순
+    context = {'posts':posts }
+    return render(request,'home/notice.html',context)
+
 
 def keyboard(request):
     return JsonResponse(
@@ -50,7 +141,7 @@ def message(request):
             }
         })
 
-    elif return_str == "HCI & 비쥬얼컴퓨팅":
+    elif return_str == "HCI&비쥬얼컴퓨팅":
         return JsonResponse({
             "message": {
                 "text": "test"
@@ -197,90 +288,48 @@ def message(request):
             }
         })
 
-def import_data(request):
-    Sheet1.objects.all().delete()
+    elif return_str == "공지사항":
+        return JsonResponse({
+            "message": {
+                "text": "세종대학교 공지사항\n\n" + notice(),
 
-    if request.method == "POST":
-        form = UploadFileForm(request.POST,
-                              request.FILES)
-
-        if form.is_valid():
-
-
-            request.FILES['file'].save_book_to_database(
-                models=[Sheet1],
-                initializers=[None],
-                mapdicts=[{'년도': 'number',
-                           '학기': 'dummy',
-                           '학수번호':'dummy',
-                           '교과목명': 'subject',
-                           '이수구분': 'dummy',
-                           '교직영역': 'dummy',
-                           '선택영역': 'dummy',
-                           '학점': 'dummy',
-                           '평가방식': 'dummy',
-                           '등급': 'dummy',
-                           '평점': 'dummy',
-                           '개설학과코드': 'dummy'}]
-            )
-            return redirect('resultTrack')
-        else:
-            return HttpResponseBadRequest()
-    else:
-        form = UploadFileForm()
-    return render(
-        request,
-        'home/upload_form.html',
-        {
-            'form': form,
-            'title': 'Import excel data into database example',
-            'header': 'Please upload sample-data.xls:'
+                "message_button": {
+                    "label": "세종대학교 공지 바로가기",
+                    "url": "http://board.sejong.ac.kr/boardlist.do?bbsConfigFK=333"
+                }
+            }
         })
 
 
-def handson_table(request):
-    return excel.make_response_from_tables(
-        [Sheet1], 'handsontable.html')
+def notice():
+    req = urllib.request.Request("http://board.sejong.ac.kr/boardlist.do?bbsConfigFK=333",
+                                 headers={'User-Agent': 'Mozilla/5.0'})
+    response = urllib.request.urlopen(req)
+    text = response.read().decode("utf8")
 
-def post_list(request):
-    return render(request, 'home/main.html')
+    soup = BeautifulSoup(text, 'html.parser')
 
+    all_infor = soup.find_all('tbody')
 
-def allTrack(request):
-    tracklists = TrackList.objects.all()
-    bsublists=BsubList.objects.all()
-    asublists=AsubList.objects.all()
+    subject = all_infor[0].find_all('td', {'class': "subject"})
+    writer = all_infor[0].find_all('td', {'class': "writer"})
+    date = all_infor[0].find_all('td', {'class': "date"})
 
-    posts = Post.objects.filter(published_date__lte=timezone.now())
+    s_str = []
+    w_str = []
+    d_str = []
+    for i in range(0, 5):
+        n = subject[i].get_text().replace("\r", "").replace("\t", "").replace("\n", "")
+        s_str.append(n)
+        m = writer[i].get_text().replace("\r", "").replace("\t", "")
+        w_str.append(m)
+        l = date[i].get_text().replace("\r", "").replace("\t", "")
+        d_str.append(l)
 
-    context= { 'posts' : posts , 'tracklists':tracklists, 'bsublists':bsublists,'asublists':asublists}
-    return render(request,'home/allTrack.html',context)
+    iflist = ""
 
-def resultTrack(request):
+    num = ["(1) ", "(2) ", "(3) ", "(4) ", "(5) "]
+    for i in range(0, len(s_str)):
+        iflist += num[i] + s_str[i] + "\n" + w_str[i] + "\n" + d_str[i] + "\n\n"
 
-    users=User.objects.all()
-    usersubs=Sheet1.objects.all()
-    tracklists = TrackList.objects.all()
-    bsublists=BsubList.objects.all()
-    asublists=AsubList.objects.all()
-
-    usersubs.update(number = request.user.get_username())  # 일괄 update 요청
-
-    context={'users':users,
-             'usersubs':usersubs,
-             'tracklists':tracklists,
-             'bsublists':bsublists,
-             'asublists':asublists,
-             }
-
-    return render(request, 'home/resultTrack.html',context)
-
-
-
-def test(request):
-    return render(request,'home/test.html')
-
-def notice(request):
-    posts = Post.objects.all().order_by('-created_date') #날짜 역순
-    context = {'posts':posts }
-    return render(request,'home/notice.html',context)
+    return iflist
